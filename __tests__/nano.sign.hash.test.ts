7c6e99635312075dc1d277b528f23a981e8c8c36
yogh333 <98384638+jcaporossi-ledger@users.noreply.github.com>
2022-10-14 14:29:59 +0200

    functionnal tests with Nano S/S+ OK

---
+++ __tests__/nano.sign.hash.test.ts
@@ -0,0 +1,60 @@
+import { LedgerSigner } from '../src/ledger-signer';
+import { Contract, number } from 'starknet';
+import { compiledOpenZeppelinAccount, getTestProvider } from './fixtures';
+import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
+import Transport from '@ledgerhq/hw-transport';
+
+const PATH = "m/2645'/1195502025'/1148870696'/0'/0'/0";
+
+describe('deploy and test Ledger secured wallet', () => {
+
+  let transport: Transport;
+
+  let signer: LedgerSigner;
+
+  let contract: Contract;
+
+  const provider = getTestProvider();
+
+  beforeAll(async () => {
+
+    transport = await TransportNodeHid.create();
+
+    signer = new LedgerSigner(PATH, transport);
+
+    const starkKeyPub = await signer.getPubKey();
+
+    const accountResponse = await provider.deployContract({
+      contract: compiledOpenZeppelinAccount,
+      constructorCalldata: [starkKeyPub],
+      addressSalt: starkKeyPub,
+    });
+
+    contract = new Contract(compiledOpenZeppelinAccount.abi, accountResponse.contract_address);
+    
+    await provider.waitForTransaction(accountResponse.transaction_hash);
+  });
+
+  afterAll(async () => {
+    await transport.close();
+  })
+
+  test('verify signature', async () => {
+
+    const msg = '0x749552d5a30f49c46e5e07f20bfc5dbe7b22cf90fcf7848b4a7ff0270400e79';
+    const signature = await signer.sign(msg, false);
+
+    const isValid = await provider.callContract({
+      contractAddress: contract.address,
+      entrypoint: 'isValidSignature',
+      calldata: [
+        number.toBN(msg).toString(),
+        '2',
+        number.toBN(signature[0]).toString(),
+        number.toBN(signature[1]).toString(),
+      ],
+    });
+
+    expect(isValid).toEqual({ result: ['0x1'] });
+  });
+});
---
+++ __tests__/nano.sign.tx.test.ts
@@ -0,0 +1,93 @@
+import { LedgerSigner } from '../src/ledger-signer';
+import { Account, Contract, number } from 'starknet';
+import { compiledOpenZeppelinAccount, compiledTestDapp, getTestProvider } from './fixtures';
+import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
+import Transport from '@ledgerhq/hw-transport';
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Before running this test, please follow theses steps:
+// 1. In a terminal, launch speculos and Nano Starknet app:  
+//      "speculos --display headless --vnc-port 41000 --api-port 5001 --apdu-port 4001 app_s.elf"
+// 2. In another terminal, launch starknet-devnet client:
+//      "starknet-devnet --seed 0"
+// 3. In another terminal, lauch the test:
+//      "yarn test __tests__/account.ledgertx-speculos.test.ts"
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+const PATH = "m/2645'/1195502025'/1148870696'/0'/0'/0";
+
+describe('deploy and test Wallet', () => {
+  let signer: LedgerSigner;
+  let transport: Transport;
+  let starkKeyPub: string;
+
+  let account: Account;
+  let dapp: Contract;
+
+  const provider = getTestProvider();
+
+  beforeAll(async () => {
+    
+    transport = await TransportNodeHid.create();
+
+    signer = new LedgerSigner(PATH, transport);
+
+    starkKeyPub = await signer.getPubKey();
+
+    const accountResponse = await provider.deployContract({
+      contract: compiledOpenZeppelinAccount,
+      constructorCalldata: [starkKeyPub],
+      addressSalt: starkKeyPub,
+    });
+
+    console.log("account address =" + accountResponse.contract_address)
+
+    account = new Account(provider, accountResponse.contract_address, signer);
+
+    await provider.waitForTransaction(accountResponse.transaction_hash);
+
+    const dappResponse = await provider.deployContract({
+      contract: compiledTestDapp,
+    });
+
+    dapp = new Contract(compiledTestDapp.abi, dappResponse.contract_address, provider)
+
+    await provider.waitForTransaction(dappResponse.transaction_hash);
+
+    let data = {
+      address: account.address,
+      amount: 1000000000000000000000
+    };
+
+    await fetch("http://127.0.0.1:5050/mint", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'}, 
+      body: JSON.stringify(data)
+    })
+
+  });
+
+  afterAll(async () => {
+    await transport.close();
+  })
+
+
+  test('execute by wallet owner', async () => {
+
+    const { transaction_hash } = await account.execute(
+      {
+        contractAddress: dapp.address,
+        entrypoint: 'set_number',
+        calldata: ['666'],
+      },
+      undefined,
+      { maxFee: '1000000000000000' }
+    );
+
+    await provider.waitForTransaction(transaction_hash);
+    
+    const response = await dapp.get_number(account.address);
+
+    expect(number.toBN(response.number as string).toString()).toStrictEqual('666');
+  });
+});
